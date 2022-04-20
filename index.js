4e627d882e67adcb98df43bced6b73a632498d70
Guille-Rubio <guillermorubiog@gmail.com>
2022-04-20 13:24:02 +0200

    ADD deployment config

--- index.js
+++ index.js
@@ -5,7 +5,7 @@ require('dotenv').config();
 require('./config/mongoConfig')
 
 const app = express()
-const port = 3000;
+const port = process.env.PORT||5000;
 
 app.use(morgan(':method :host :status :param[id] - :response-time ms :body'));
 app.use(express.json())
