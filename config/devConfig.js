b3d10b08a76386372f7a0c8a81985dedec5bd457
Guille-Rubio <guillermorubiog@gmail.com>
2022-04-20 16:08:21 +0200

    FIXED port for deployment

--- config/devConfig.js
+++ config/devConfig.js
@@ -4,6 +4,6 @@ module.exports = {
     MONGODB_DBNAME: NasaAPI,
     MONGODB_CLUSTER: nasaapi.zovxd,
     NASA_API_KEY: kGUbR4WXdvFbZVozL3M5V3iCG5xsr6xxRVYGsbKB,
-    PORT: 3000
+    PORT: 5000
 
 }
