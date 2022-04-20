c5b368018ce1f658a0008095d740699e2b8f67c3
Guille-Rubio <guillermorubiog@gmail.com>
2022-04-20 17:10:04 +0200

    ADD path /

--- index.js
+++ index.js
@@ -5,13 +5,16 @@ require('dotenv').config();
 require('./config/mongoConfig')
 
 const app = express()
-const port = process.env.PORT||5000;
+const port = process.env.PORT || 5000;
 
 app.use(morgan(':method :host :status :param[id] - :response-time ms :body'));
 app.use(express.json())
 app.use(express.urlencoded({ extended: true }));
 
 app.use("/api", router);
+app.use("/", (req, res) => {
+    res.status(400).json({ msg: "Bienvenido a la API" })
+})
 app.use((req, res, next) => {
     return res.status(404).send({ message: 'Route' + req.url + ' Not found.' });
 });
