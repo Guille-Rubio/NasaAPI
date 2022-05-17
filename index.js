ae32e2762492fdb7be77c542d57a2c2f64d1d65d
Guille-Rubio <guillermorubiog@gmail.com>
2022-05-17 21:55:30 +0200

    FIX routes path

--- index.js
+++ index.js
@@ -1,6 +1,6 @@
 const express = require('express');
 const userRouter = require('./routes/userRoutes');
-const apiRouter = require('./routes/APIRoutes');
+const apiRouter = require('./routes/APIroutes');
 require('dotenv').config();
 require('./config/mongoConfig');
 const { connectSQL } = require('./config/elephantSQLConfig');
